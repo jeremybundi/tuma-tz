9e094d1f4ccd4c9fdf152e6b224d762a895b8a01
jeremy <jeremybundi4@gmail.com>
2025-05-23 22:19:05 +0300

    mobile view and navbar

--- src/app/dashboard/Home.tsx
+++ src/app/dashboard/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from './components/NavBar'; 
+//import NavBar from './components/NavBar'; 
 import Section from './components/Section'; 
 import Mission from './components/Mission'; 
 import HowItWorks from './components/HowItWorks'; 
